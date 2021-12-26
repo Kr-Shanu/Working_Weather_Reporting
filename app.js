a58918525e7f0dd583a9536def547fb6441966d6
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2021-12-26 19:47:16 +0530

    ejs rocks, only left to setup failure page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Using time from the system.
-var today = new Date();
-var hours = today.getHours();
-var frame = "AM";
+let today = new Date();
+let hours = today.getHours();
+let frame = "AM";
 if(hours > 12)
 {
     hours = hours-12;
     frame = "PM";
 }
-var time = hours + ":" + today.getMinutes() + ":" + today.getSeconds()+ " "+frame;
+let time = hours + ":" + today.getMinutes() + ":" + today.getSeconds()+ " "+frame;
 
 
 const express = require("express");
@@ -17,6 +17,8 @@ const bodyParser = require("body-parser");
 const http = require("https");
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -40,7 +42,7 @@ app.post("/", function (request, res) {
     const unit = "metric";
     // Api key is required for authentication
 
-    const apiKey = "6615d06477e51d724c56a4bb0256b5c3"
+    const apiKey = "" // ☠️☠️❌❌❌ here you need to enter your api key from open weather api to make it run ❌❌❌
 
     // This url is the url for the open weather api without the other attributes
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + unit;
@@ -50,35 +52,23 @@ app.post("/", function (request, res) {
     // Getting information using https get  function using the above url.
     http.get(url, function (response) 
     {
+        console.log("Status code :"+ res.statusCode);
         console.log("Https is working");
 
         response.on("data", function (data) 
         {
+            const city_name = city;
             const weather = JSON.parse(data);
             const temperature = weather.main.temp;
-            const wind = weather.wind.speed;
+            const wind = Math.floor((weather.wind.speed)*3.60);
             const description = weather.weather[0].description;
-            res.write("<h1>The place entered by you is : "+ city +"</h1>");
-            res.write("<h3>Wind = " + wind +" </h3>");
-            res.write("<h3>Temperature = " + temperature + "</h3>");
-            res.write("<h3>Description :"  + description + "</h3>");
-            res.send();
-            // const imgIcon = weather.weather[0].icon;
-            // Trying to fetch the dom elements first in order to make some changes in there.
-            // const imageUrl = "http://openweathermap.org/img/w/"+imgIcon+".png";
+            const imgIcon = weather.weather[0].icon;
+            const imageUrl = "http://openweathermap.org/img/w/"+imgIcon+".png";
+            res.render("show_weather", {dc : temperature, speed : wind, cities: city_name,desc: description, icon: imageUrl});
         })
         console.log("Process finished!!!");       
-        // res.sendFile(__dirname+"/success.html");
     });
-    // console.log(city);
 });
-
-
-app.get("/success", function(req, res)
-{
-    res.sendFile(__dirname+"/success.html");
-});
-
 
 
 app.post("/success", function(req, res)
